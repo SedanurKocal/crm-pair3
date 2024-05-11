acc4e051b250bd4f34c9d949fdf0c402d234eb75
OĞUZHAN SEÇGEL <osecgel@hotmail.com>
2024-05-11 12:45:47 +0300

    yaml dosyaları ve docker compose / docker file configleri hazırlandı.

--- addressservice/src/main/java/com/tcellpair3/addressservice/AddressserviceApplication.java
+++ addressservice/src/main/java/com/tcellpair3/addressservice/AddressserviceApplication.java
@@ -2,7 +2,7 @@ package com.tcellpair3.addressservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class AddressserviceApplication {
 
