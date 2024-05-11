a10ca73fff84947d9baaeeed0829db450fc70a6c
OĞUZHAN SEÇGEL <osecgel@hotmail.com>
2024-05-11 12:24:02 +0300

    docker compose

---
+++ authserver/src/test/java/com/tcellpair3/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcellpair3.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ discoveryserver/src/test/java/com/tcellpair3/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcellpair3.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
