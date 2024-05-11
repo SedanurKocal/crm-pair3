3e904be7044091f0ee272a32c2b56e541e7bd4cd
OĞUZHAN SEÇGEL <osecgel@hotmail.com>
2024-05-12 00:26:58 +0300

    current

--- authserver/src/main/java/com/tcellpair3/authserver/AuthserverApplication.java
+++ authserver/src/main/java/com/tcellpair3/authserver/AuthserverApplication.java
@@ -2,7 +2,7 @@ package com.tcellpair3.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class AuthserverApplication {
 
