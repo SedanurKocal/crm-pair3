acec364f3f04caa658f74d3b47009659d1f46eb2
OĞUZHAN SEÇGEL <osecgel@hotmail.com>
2024-06-02 18:59:38 +0300

    productservice identity/swagger config impl

--- productservice/src/main/java/com/tcellpair3/productservice/ProductserviceApplication.java
+++ productservice/src/main/java/com/tcellpair3/productservice/ProductserviceApplication.java
@@ -1,10 +1,11 @@
 package com.tcellpair3.productservice;
 
+import com.turkcell.tcell.core.annotations.EnableSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@EnableSecurity
 public class ProductserviceApplication {
 
 	public static void main(String[] args) {
---
+++ productservice/src/main/java/com/tcellpair3/productservice/core/configuration/ProductSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.tcellpair3.productservice.core.configuration;
+
+import com.turkcell.tcell.core.security.BaseSecurityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class ProductSecurityConfiguration {
+
+    private final BaseSecurityService baseSecurityService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        baseSecurityService.configureCommonSecurityRules(http);
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                        .anyRequest().permitAll()
+        );
+        return http.build();
+    }
+}
